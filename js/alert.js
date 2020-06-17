9784b0d558985028efb6baa019fdee05170dad19
Vlad <vladyslavlu@gmail.com>
2020-06-17 19:22:47 +0300

    add new

---
+++ js/alert.js
@@ -0,0 +1,113 @@
+"Use strict";
+
+// let message = 'Hello!';
+
+// alert( message );
+
+// let user = 'John';
+// let age = 25;
+// let message = 'Hello';
+
+// let message;
+
+// message = 'Hello';
+
+// message = 'World';
+
+// alert( message );
+
+// let admin, name;
+
+// name = 'Джон';
+
+// admin = name;
+
+// alert( admin );
+
+// let ourPlanetName = 'Земля';
+
+// let currentUserName = 'Джон';
+
+// let name = prompt('Как тебя зовут?', '');
+
+// alert(name);
+
+
+
+// let value = prompt('Какое «официальное» название JavaScript?', '');
+
+// if (value == 'ECMAScript') {
+//     alert('Верно!');
+// } else {
+//     alert('Не знаете? ECMAScript!');
+// }
+
+
+
+// let value = prompt('Введите число', '');
+
+// if (value > 0 ) {
+//     alert( 1 );
+// } else if (value < 0) {
+//     alert( -1 );
+// } else {
+//     alert( 0 );
+// }
+
+// let message = (login == 'Сотрудник') ? 'Привет' :
+//     (login == 'Директор') ? 'Здравствуйте' :
+//     (login == '') ? 'Нет логина' :
+//     '';
+
+
+// let userName = prompt("Кто там?", '');
+
+// if (userName == 'Админ') {
+
+//   let pass = prompt('Пароль?', '');
+
+//   if (pass == 'Я главный') {
+//     alert( 'Здравствуйте!' );
+//   } else if (pass == '' || pass == null) {
+//     alert( 'Отменено' );
+//   } else {
+//     alert( 'Неверный пароль' );
+//   }
+
+// } else if (userName == '' || userName == null) {
+//   alert( 'Отменено' );
+// } else {
+//   alert( "Я вас не знаю" );
+// }
+
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//       console.log( i );
+//     }
+//   }
+
+
+// for (let i = 0; i < 3; i++) {
+//     alert( `number ${i}!` );
+//   }
+
+//   let i = 0;
+//   while (i < 3) {
+//       console.log( `number ${i}!` );
+//       i++;
+//   }
+
+
+// let num;
+
+// do {num = promt('Введите число больше 100', 0);
+// } while (num <= 100 && num);
+
+
+
+    
+
+
+
+
