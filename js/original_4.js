47e867890736d8647a75c34461c76521f7bb5868
Vlad <vladyslavlu@gmail.com>
2020-06-29 18:51:06 +0300

    Change js src in index.html. Made task 4

--- js/original_4.js
+++ js/original_4.js
@@ -56,10 +56,38 @@ const personalMovieDB = {
             console.log(personalMovieDB);
         }
     },
-    writeYourGenres: function() {
-        for (let i = 1; i <= 3; i++) {
-            personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
         }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i < 2; i++) {
+            // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+            // if(genre === '' || genre == null) {
+            //     console.log('Вы ввели некорректные данные или не ввели их вовсе');
+            //     i--;
+            // } else {
+            //     personalMovieDB.genres[i - 1] = genre;
+            // }  
+
+            let genres = prompt(`Введите ваши любимые жанры через запятую`).toLocaleLowerCase();
+
+            if(genres === '' || genres == null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres = genres.split(', ');
+                personalMovieDB.genres.sort();
+            }  
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });  
     }
 };
 
